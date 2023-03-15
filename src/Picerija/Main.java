09658efbeb49e885ebc48ab0f2ecdc0d09c602e0
Klaiva <maris.klaiva@gmail>
2023-03-15 15:58:50 +0200

    1

--- src/Picerija/Main.java
+++ src/Picerija/Main.java
@@ -1,9 +1,37 @@
 package Picerija;
 
+import javax.swing.JOptionPane;
+
 public class Main {
+	static void pasutijums(){
+		
+	}
+	
+	static void uzvietas(){
+		
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		String[] options = {"Pasutijums","Uz vietas","stop"};
+		String izvele;
+		do{
+			izvele=(String)JOptionPane.showInputDialog(null,"Izvelies ka edisiet picu?","How?",JOptionPane.QUESTION_MESSAGE,null, options,options[0]);
+			switch(izvele){
+			case"Pasutijums":
+				pasutijums();
+				break;
+				
+			case"Uz vietas":
+				uzvietas();
+				break;
+				
+			case"stop":
+				JOptionPane.showMessageDialog(null, "Programa tik apturçta");
+				System.exit(0);
+			}
+			
+		}while(!izvele.equalsIgnoreCase("stop"));
+		
 
 	}
 
