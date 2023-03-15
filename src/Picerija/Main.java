a75281c49d52201c1a8a906b91f5ec2ba3081605
Klaiva <maris.klaiva@gmail>
2023-03-15 15:32:31 +0200

    sakums

---
+++ src/Picerija/Main.java
@@ -0,0 +1,10 @@
+package Picerija;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/Picerija/Pica.java
@@ -0,0 +1,5 @@
+package Picerija;
+
+public class Pica {
+
+}
