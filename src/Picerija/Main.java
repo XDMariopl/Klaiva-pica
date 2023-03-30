99c32407af86409c19e80423ce36d38d41bf2125
Maris Klaiva <maris.klaiva@gmail>
2023-03-30 09:52:59 +0300

    cenas veiksmīgi ievadas čeka

--- src/Picerija/Main.java
+++ src/Picerija/Main.java
@@ -1,33 +1,35 @@
 package Picerija;
 
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 
 import javax.swing.JOptionPane;
 
 public class Main {
+	static DecimalFormat df= new DecimalFormat("0.00");
 	static String[]yesOrNo={"Ja","Ne"};
 	static String[]size={"small","medium","large"};
 	static String[]topings={"-","Peperoni","mocorelas siers"};
-	static String[]whatpica={"Vezuva","Margarita","Havajiesu"};
+	static String[]whatpica={"Vezuva","Margarita","Havajiešu"};
 	static String[]drinks={"-","Coca-Cola","Sprite","Fanta"};
 	static String picaslieums,piedevas,dzeriens,pizzaName;
 	static ArrayList<Pica> pizza = new ArrayList<Pica>();
 	static int pizzaCount=0;
 	static boolean vienadas=true;
-	static int cikpicas;
+	static int cikpicas,i=0;
 	
 	static void picasPas(){
 		pizzaName=(String) JOptionPane.showInputDialog(null,"Vezuva 10.00 Eur| Margarita 9.00 Eur| Hvajiesu 11.00 Eur","Pica",JOptionPane.QUESTION_MESSAGE,null,whatpica,whatpica[0]);
-		picaslieums=(String) JOptionPane.showInputDialog(null,"Izvelies picas lielumu","Lielums",JOptionPane.QUESTION_MESSAGE,null,size,size[1]);
-		piedevas=(String) JOptionPane.showInputDialog(null,"Izvelies picas piedevas","Piedevas", JOptionPane.QUESTION_MESSAGE,null,topings,topings[0]);
-		dzeriens=(String) JOptionPane.showInputDialog(null,"Izvelies dzerienu","Dzerieni", JOptionPane.QUESTION_MESSAGE,null,drinks,drinks[0]);
+		picaslieums=(String) JOptionPane.showInputDialog(null,"Small -2.00 Eur| Medium +0.00 Eur| Large +4.00 Eur","Lielums",JOptionPane.QUESTION_MESSAGE,null,size,size[1]);
+		piedevas=(String) JOptionPane.showInputDialog(null,"Peperoni +2.40 Eur| Mocorelas siers +2.20 Eur|","Piedevas", JOptionPane.QUESTION_MESSAGE,null,topings,topings[0]);
+		dzeriens=(String) JOptionPane.showInputDialog(null,"Coca-Cola +2.00 Eur| Sprite +1.85 Eur| Fanta +1.90 Eur","Dzerieni", JOptionPane.QUESTION_MESSAGE,null,drinks,drinks[0]);
 		pizza.add(new Pica(pizzaName,picaslieums,piedevas,dzeriens));
 	}
 	
 	static void pasutijums(){
 		String adrese=(String)JOptionPane.showInputDialog("Ievadi uz kuru adresi sutisiet picu.");
 		String StrCheck="";
-		int i=0;
+		double sum=0;
 		do{
 		cikpicas=Integer.parseInt(JOptionPane.showInputDialog("Cik picas pasutisiet?"));
 		}while(cikpicas<=0);
@@ -39,20 +41,24 @@ public class Main {
 		if(vienadas==true){
 			picasPas();
 			StrCheck+=pizza.get(pizzaCount).check();
+			sum=pizza.get(pizzaCount).picaSumma()*cikpicas;
 			pizzaCount++;
+			JOptionPane.showMessageDialog(null, StrCheck+"| *"+cikpicas+"\nbez PVN:"+sum+"\nar PVN:"+df.format(sum+(sum*0.21))+"\n"+adrese);
 		}else{
-			for(i=0;i<cikpicas;i++){
+			for(i=0+pizzaCount;i<cikpicas;i++){
 				picasPas();
-				StrCheck+=pizza.get(i).check()+"\n";
+				sum+=pizza.get(i).picaSumma();
+				StrCheck+=pizza.get(i).check()+" | "+pizza.get(i).picaSumma()+"\n";
 			}
 			pizzaCount+=i;
+			JOptionPane.showMessageDialog(null, StrCheck+"\nbez PVN: "+sum+"\nar PVN:"+df.format(sum+(sum*0.21))+"\n"+adrese);
 		}
-		JOptionPane.showMessageDialog(null, StrCheck+"\n"+adrese);
 	}
-	
 	static void uzvietas(){
-		do {
-			cikpicas=Integer.parseInt(JOptionPane.showInputDialog("Cik picas pasutisiet?"));
+		String StrCheck="";
+		double sum=0;
+		do{
+		cikpicas=Integer.parseInt(JOptionPane.showInputDialog("Cik picas pasutisiet?"));
 		}while(cikpicas<=0);
 		if(cikpicas!=1){
 			if(JOptionPane.showInputDialog(null,"Vai visas picas bus vienadas","?",JOptionPane.QUESTION_MESSAGE,null,yesOrNo,yesOrNo[0])=="Ne"){
@@ -61,11 +67,19 @@ public class Main {
 		}
 		if(vienadas==true){
 			picasPas();
+			StrCheck+=pizza.get(pizzaCount).check();
+			sum=pizza.get(pizzaCount).picaSumma()*cikpicas;
+			pizzaCount++;
+			JOptionPane.showMessageDialog(null, StrCheck+"| *"+cikpicas+"\nbez PVN: "+sum+"\nar PVN:"+df.format(sum+(sum*0.21)));
 		}else{
-			for(int i=0;i<cikpicas;i++){
+			for(i=0+pizzaCount;i<cikpicas;i++){
 				picasPas();
+				sum+=pizza.get(i).picaSumma();
+				StrCheck+=pizza.get(i).check()+" | "+pizza.get(i).picaSumma()+"\n";
 			}
-		}	
+			pizzaCount+=i;
+			JOptionPane.showMessageDialog(null, StrCheck+"\nbez PVN: "+sum+"\nar PVN: "+df.format(sum+(sum*0.21)));
+		}
 	}
 
 	public static void main(String[] args) {
--- src/Picerija/Pica.java
+++ src/Picerija/Pica.java
@@ -14,13 +14,60 @@ public class Pica {
 	}
 	String check(){
 		String str="Pica: "+picaName+" "+picaSize+" ";
+		
 		if(!picaTopings.equals("-")) {
-			str+=picaTopings+" |\n";
+			str+=picaTopings+" |";
 		}
 		if(!Drinks.equals("-")) {
-			str+=Drinks+"|";
+			str+=Drinks+" |";
 		}
 		return str;
+	}
+	
+	double picaSumma(){
+		double sum=0;
+		switch(picaName) {
+		case "Vezuva":
+			sum+=10;
+			sum+=sizeSumma()+toppingSumma()+drinkSumma();
+		break;
+		case "Margarita":
+			sum+=9;
+			sum+=sizeSumma()+toppingSumma()+drinkSumma();
+		break;
+		case "Havajiešu":
+			sum+=11;
+			sum+=sizeSumma()+toppingSumma()+drinkSumma();
+		break;
+		}
+		return sum;
+	}
+	double sizeSumma(){
+		double sum=0;
+		switch(picaSize) {
+		case "small":	sum-=2;	break;
+		case "large":	sum+=4;	break;
+		}
+		return sum;
+	}
+	
+	double toppingSumma(){
+		double sum=0;
+		switch(picaTopings) {
+		case "Peperoni":	sum+=2.40;	break;
+		case "mocorelas siers":	sum+=2.20;	break;
+		}
+		return sum;
+	}
+	
+	double drinkSumma(){
+		double sum=0;
+		switch(Drinks) {
+		case "Coca-Cola":	sum+=2;	break;
+		case "Sprite":	sum+=1.85;	break;
+		case "Fanta":	sum+=1.9;	break;
+		}
+		return sum;
 	}
 
 }
